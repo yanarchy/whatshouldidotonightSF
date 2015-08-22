432c351e373807985164b1acdf073396713ba54a
Yan Fan <theyanfan@gmail.com>
2015-08-22 12:58:27 -0700

    Changed a small bit in index.js

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 var express = require('express');
+var request = require('request');
+
 var app = module.exports = express();
 
 app.set('port', (process.env.PORT || 3000));
@@ -6,12 +8,11 @@ app.set('port', (process.env.PORT || 3000));
 app.use(express.static(__dirname + '/app'));
 app.set('app', __dirname + '/app');
 
-require('./routes')(app);
 
 app.get('/', function (req, res) {
   response.render('')
 });
 
 app.listen(app.get('port'), function() {
-  console.log('App listening at http://%s:%s', app.get('port'));
+  console.log('App listening at http://localhost:', app.get('port'));
 });
